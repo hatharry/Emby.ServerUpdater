a6c53b0b0df7c1e0044e2a3a28ae2a81c8decb5b
hatharry <hatharry@hotmail.com>
2018-07-22 21:57:03 +1200

    Update Url

--- EmbyServiceUpdater.ps1
+++ EmbyServiceUpdater.ps1
@@ -33,7 +33,7 @@ Class EmbyServiceUpdater {
     }
 
     getLatestRelease() {
-        $releases = ConvertFrom-Json (Invoke-WebRequest "https://api.github.com/repos/mediabrowser/emby/releases" -UseBasicParsing)
+        $releases = ConvertFrom-Json (Invoke-WebRequest "https://api.github.com/repos/mediabrowser/Emby.Releases/releases" -UseBasicParsing)
         $preRelease = $this.localVersion.Revision -ne 0
         $this.release = ($releases | Where-Object {$_.prerelease -eq $preRelease} | Select-Object -first 1) 
     }
