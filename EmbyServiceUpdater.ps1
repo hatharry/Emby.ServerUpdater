0adf58c93b34c6348d87b4d57cfc074269dd7402
hatharry <hatharry@hotmail.com>
2019-04-19 01:06:09 +1200

    pin to release

--- EmbyServiceUpdater.ps1
+++ EmbyServiceUpdater.ps1
@@ -33,7 +33,7 @@ Class EmbyServiceUpdater {
 
     getLatestRelease() {
         $releases = ConvertFrom-Json (Invoke-WebRequest "https://api.github.com/repos/mediabrowser/Emby.Releases/releases" -UseBasicParsing)
-        $preRelease = $this.localVersion.Revision -ne 0
+        $preRelease = $false
         $this.release = ($releases | Where-Object {$_.prerelease -eq $preRelease} | Select-Object -first 1) 
     }
 
